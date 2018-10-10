d097bea7dab5543a0ff2588e2f5088539035f5b2
Tamas Santa <tamas.santa@infinityworks.com>
2018-10-10 13:46:46 +0100

    wip tagger

--- tagger.sh
+++ tagger.sh
@@ -26,22 +26,57 @@ rootbranch="master"
 #   - get the tag of the _first_ commit in history
 #   - tag the rest with {first-commit-tag}-{branch-name}-{version} (in reverse)
 
-# Implementation
-rc=$(git log --walk-reflogs master --pretty=format:"%h" --no-patch)
-rootcommits=($rc) # I don't know why we need this at all, but this will result in an array of lines
+# $arg1 -> branch name
+# $arg2 -> version
+# $arg3 -> parent tag
+get_tag_name() {
 
-# echo ${#rootcommits[@]}
-# echo "---- Listing commits from ${rootbranch} ----"
+    if [ $1 == $rootbranch ]; then
+        echo "v0.$2"
+    else
+        tag="$3-$1"
 
-version=1
-for ((i=${#rootcommits[@]}-1; i>=0; i--)); do
-  #git tag -a -m "auto-generated" v0.${version} ${rootcommits[$i]}
-  currenttags=$(git describe --tags ${rootcommits[$i]})
+        if [ ${tag} ]; then
+            tag="$tag-$2"
+            echo $tag
+        fi
+    fi
+}
 
-  if [ -z ${currenttags} ]; then
-    git tag -a -m "auto-generated" v0.${version} ${rootcommits[$i]}
-  else
-    echo "Commit has been tagged already - ${currenttags} - ${rootcommits[$i]}"
-  fi
-  version=$((version + 1))
+
+# Tag all commits in a branch with version numbers
+# $arg1 -> branch name
+tag_branch() {
+    echo "Tagging branch [$1]"
+    commits=$(git log --walk-reflogs $1 --pretty=format:"%h" --no-patch); commits=($commits)
+    version=1
+    parenttag=""
+
+    if [ "$1" != "$rootbranch" ]; then
+        parenttag=$(git describe --tags ${commits[${#commits[@]}-1]})
+    fi
+
+    for ((i=${#commits[@]}-1; i>=0; i--)); do
+        tag=$(git describe --tags ${commits[$i]})
+
+        if [ -z ${tag} ]; then
+            tag=get_tag_name $1 ${version} ${parenttag}
+            git tag -a -m "auto-generated" ${tag} ${commits[$i]}
+        else
+            echo "Commit has been tagged already - ${tag} - ${commits[$i]}"
+        fi
+        version=$((version + 1))
+    done
+}
+
+tag_branch $rootbranch
+
+# ================ Tagging child branches =================
+branches=$(git for-each-ref --format='%(refname:short)' refs/heads)
+branchlist=($branches)
+
+for ((i=${#branchlist[@]}-1; i>=0; i--)); do
+    if [ ${branchlist[$i]} != $rootbranch ]; then
+        tag_branch ${branchlist[$i]}
+    fi
 done
